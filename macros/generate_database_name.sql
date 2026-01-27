648bd0681d0d1432378610e28b583018421cb4d4
Orit Barak <orit.b@bst.co.il>
2026-01-27 09:21:45 +0000

    macro

---
+++ macros/generate_database_name.sql
@@ -0,0 +1,32 @@
+target. Database {% macro generate_database_name(custom_database_name=none, node=none) -%}
+ 
+  {%- set target_name = target.name -%}
+ 
+  {% if target_name == 'DEV' %}
+      DEVELOPMENT
+  {% elif target_name == 'UAT' %}
+     {# Non-dev, non-ci environments use folder-based database logic #}
+      {% set file_path = node.original_file_path %}
+      {% if 'silver' in file_path  %}
+          SILVER_UAT
+      {% elif 'gold' in file_path  %}
+          GOLD_UAT
+      {% elif 'datalake' in file_path  %}
+          BRONZE_UAT
+      {% else %}
+          {{target.database}} {# Default fallback to target database #}
+      {% endif %}
+  {% else %}
+      {# Non-dev, non-ci environments use folder-based database logic #}
+      {% set file_path = node.original_file_path %}
+      {% if 'silver' in file_path  %}
+          SILVER_PROD
+      {% elif 'gold' in file_path  %}
+          GOLD_PROD
+      {% elif 'datalake' in file_path  %}
+          BRONZE_PROD
+      {% else %}
+          {{target.database}} {# Default fallback to target database #}
+      {% endif %}
+  {% endif %}
+{% endmacro %}
