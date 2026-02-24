192cb643edcddb26a11a62c3598092fbb512e52a
Orit Barak <orit.b@bst.co.il>
2026-02-24 16:30:33 +0000

    uat

--- macros/generate_database_name.sql
+++ macros/generate_database_name.sql
@@ -1,32 +1,30 @@
-target. Database {% macro generate_database_name(custom_database_name=none, node=none) -%}
- 
-  {%- set target_name = target.name -%}
- 
-  {% if target_name == 'dev' %}
-      DEVELOPMENT
-  {% elif target_name == 'UAT' %}
-     {# Non-dev, non-ci environments use folder-based database logic #}
-      {% set file_path = node.original_file_path %}
-      {% if 'silver' in file_path  %}
-          SILVER_UAT
-      {% elif 'gold' in file_path  %}
-          GOLD_UAT
-      {% elif 'bronze' in file_path  %}
-          BRONZE_UAT
-      {% else %}
-          {{target.database}} {# Default fallback to target database #}
-      {% endif %}
-  {% else %}
-      {# Non-dev, non-ci environments use folder-based database logic #}
-      {% set file_path = node.original_file_path %}
-      {% if 'silver' in file_path  %}
-          SILVER_PROD
-      {% elif 'gold' in file_path  %}
-          GOLD_PROD
-      {% elif 'bronze' in file_path  %}
-          BRONZE_PROD
-      {% else %}
-          {{target.database}} {# Default fallback to target database #}
-      {% endif %}
-  {% endif %}
-{% endmacro %}
+{% macro generate_database_name(custom_database_name=none, node=none) -%}
+    {%- set target_name = target.name -%}
+    {%- set file_path = node.original_file_path | lower -%}
+
+    {% if target_name == 'dev' %}
+        DEVELOPMENT
+
+    {% elif target_name == 'UAT' %}
+        {% if 'silver' in file_path %}
+            SILVER_UAT
+        {% elif 'gold' in file_path %}
+            GOLD_UAT
+        {% elif 'bronze' in file_path %}
+            BRONZE_UAT
+        {% else %}
+            {{ target.database }}
+        {% endif %}
+
+    {% else %}
+        {% if 'silver' in file_path %}
+            SILVER_PROD
+        {% elif 'gold' in file_path %}
+            GOLD_PROD
+        {% elif 'bronze' in file_path %}
+            BRONZE_PROD
+        {% else %}
+            {{ target.database }}
+        {% endif %}
+    {% endif %}
+{%- endmacro %}
