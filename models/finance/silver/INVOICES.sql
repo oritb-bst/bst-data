7aac2402018e9188844f24ec483b0cb98521fe2f
Orit Barak <orit.b@bst.co.il>
2026-03-03 12:12:06 +0000

    silver projcat and test

--- models/finance/silver/INVOICES.sql
+++ models/finance/silver/INVOICES.sql
@@ -1,2 +1,8 @@
-select *
+select 
+    INVOICE_NUMBER as "מספר חשבנית",
+    DOC_PROJECT as "מספר פרויקט",
+    INVOICE_DATE as "תאריך חשבונית",
+    TOTAL_PRICE as "סכום חשבונית",
+    id 
+
 from {{ ref('INVOICES_STG') }}
