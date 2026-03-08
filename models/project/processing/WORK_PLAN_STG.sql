8f8329d61482096a823ada0fef91a4a4f29d92ce
Orit Barak <orit.b@bst.co.il>
2026-03-08 09:06:51 +0000

    DATE CHANGES

--- models/project/processing/WORK_PLAN_STG.sql
+++ models/project/processing/WORK_PLAN_STG.sql
@@ -1,3 +1,14 @@
 SELECT
-   *
+   PROJECT_NAME,
+   PROJECT_NUMBER,
+   TO_CHAR(
+        DATE_FROM_PARTS(
+            YEAR(CURRENT_DATE()),
+            MONTH(MONTH_WORK_PLAN),
+            1
+        ),
+        'MM/YY'
+   ) AS MONTH_YEAR,
+	REVENUE,
+	EXPENSE
 FROM {{ source('csv', 'WORK_PLAN') }}
