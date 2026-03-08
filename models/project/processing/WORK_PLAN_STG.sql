035d65a6fdb4ec195fea54abb91a34ae57a741ff
Orit Barak <orit.b@bst.co.il>
2026-03-08 15:04:43 +0000

    MONTH_WORK_PLAN

--- models/project/processing/WORK_PLAN_STG.sql
+++ models/project/processing/WORK_PLAN_STG.sql
@@ -1,14 +1,7 @@
 SELECT
    PROJECT_NAME,
    PROJECT_NUMBER,
-   TO_CHAR(
-        DATE_FROM_PARTS(
-            YEAR(CURRENT_DATE()),
-            MONTH(MONTH_WORK_PLAN),
-            1
-        ),
-            'DD/MM/YYYY'                    
-   ) AS MONTH_YEAR,
-	REVENUE,
-	EXPENSE
+   MONTH_WORK_PLAN,
+   REVENUE,
+   EXPENSE
 FROM {{ source('csv', 'WORK_PLAN') }}
