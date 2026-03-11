c07690f6718308c94badc38fdc223c2c3eea0c71
Orit Barak <orit.b@bst.co.il>
2026-03-11 10:02:51 +0000

    MONTH_WORK_PLAN ::DATE

--- models/project/processing/WORK_PLAN_STG.sql
+++ models/project/processing/WORK_PLAN_STG.sql
@@ -1,14 +1,7 @@
 SELECT
    PROJECT_NAME,
    PROJECT_NUMBER,
-    TO_CHAR(
-        DATE_FROM_PARTS(
-            2000 + TO_NUMBER(RIGHT(WORK_PLAN.MONTH_WORK_PLAN,2)),  -- שנה
-            TO_NUMBER(LEFT(WORK_PLAN.MONTH_WORK_PLAN,2)),          -- חודש
-            1                                                      -- יום
-        ),
-        'DD/MM/YYYY'
-   )  ::DATE AS MONTH_WORK_PLAN,
+   MONTH_WORK_PLAN ::DATE AS  MONTH_WORK_PLAN,
    REVENUE,
    EXPENSE
 FROM {{ source('csv', 'WORK_PLAN') }}
