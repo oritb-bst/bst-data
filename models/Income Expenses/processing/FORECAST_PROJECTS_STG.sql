4beac972f3271dc2bdd335b1267641a7a435a9d4
Orit Barak <orit.b@bst.co.il>
2026-03-26 11:50:20 +0000

    forecast_revenue_new_ forecast_expense_new,

--- models/Income Expenses/processing/FORECAST_PROJECTS_STG.sql
+++ models/Income Expenses/processing/FORECAST_PROJECTS_STG.sql
@@ -6,6 +6,8 @@ select
     STATDES as status_description,
     EARNCAST/1000::decimal(18,2) as forecast_revenue,
     TOTEXPENSE/1000::decimal(18,2) as forecast_expense,
+    EARNCAST_NEW as forecast_revenue_new,
+    TOTEXPENSE_NEW as forecast_expense_new,
     FOREX as id,
     LOAD_TS
 from {{ source('bronze', 'FORECAST_PROJECTS') }}
