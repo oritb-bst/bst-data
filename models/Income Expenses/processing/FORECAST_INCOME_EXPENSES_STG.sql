68ec0d81f2b493b76cb0e719498e5a2c88af391e
Orit Barak <orit.b@bst.co.il>
2026-03-26 13:47:04 +0000

    forecast_revenue_new,     forecast_expense_new,

--- models/Income Expenses/processing/FORECAST_INCOME_EXPENSES_STG.sql
+++ models/Income Expenses/processing/FORECAST_INCOME_EXPENSES_STG.sql
@@ -1,6 +1,6 @@
 select
     TO_DATE('01/' || FORECAST_INCOME_EXPENSES.FORDATE , 'DD/MM/YY') ::DATE AS forecast_month,
     PROJDOCNO as project_doc_no,
-    EARNCAST/1000::decimal(18,2) as forecast_revenue_new,
-    TOTEXPENSE/1000::decimal(18,2) as forecast_expense_new,
+    forecast_revenue_new,
+    forecast_expense_new,
 from {{ source('bronze', 'FORECAST_INCOME_EXPENSES') }}
