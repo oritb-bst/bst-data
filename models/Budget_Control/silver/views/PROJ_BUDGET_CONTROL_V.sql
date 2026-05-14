cdb10beb2df201eac65269eaabd6befee65c95d6
Orit Barak <orit.b@bst.co.il>
2026-05-14 14:05:39 +0000

    changes PROJ_BUDGET_CONTROL

--- models/Budget_Control/silver/views/PROJ_BUDGET_CONTROL_V.sql
+++ models/Budget_Control/silver/views/PROJ_BUDGET_CONTROL_V.sql
@@ -5,5 +5,5 @@ select
     BUD_CONTROL_MONTH     as "חודש בקרה",
     BUD_CONTROL_DATE      as "תאריך בקרה",
     CURVERSION            as "מהדורה נוכחית",
-    SOURCE_DB             as "חברה"
+    SOURCE_DB              as "חברה"
 from {{ ref('PROJ_BUDGET_CONTROL') }}
