e522e891c7e7baaa7b127b68666d0ee9dfe0911b
Orit Barak <orit.b@bst.co.il>
2026-05-17 12:51:41 +0000

    added fields to DIM_PROJECTS table for budget control model

--- models/general_entity/silver/DIM_PROJECTS.sql
+++ models/general_entity/silver/DIM_PROJECTS.sql
@@ -5,5 +5,7 @@ select
     PROJMANG,
     PROJTYPECODE,
     PROJTYPEDES,
+    BSA_SIZESUM,
+    BSA_APARTSUM,
     SOURCE_DB
 from {{ ref('DIM_PROJECTS_STG') }}
--- models/general_entity/silver/views/DIM_PROJECTS_V.sql
+++ models/general_entity/silver/views/DIM_PROJECTS_V.sql
@@ -5,5 +5,7 @@ select
     PROJMANG as "שם מנהל פרויקט",
     PROJTYPECODE as "קוד סוג פרויקט",
     PROJTYPEDES as "תאור סוג פרויקט",
+    BSA_SIZESUM as "סך הכל מטר רבוע לפרויקט",
+    BSA_APARTSUM as "מספר יחידות דיור",
     SOURCE_DB  as "חברה"
 from {{ ref('DIM_PROJECTS') }}
