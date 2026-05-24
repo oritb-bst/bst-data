a34aa4740fe585fc4728bea91e3e8725f7f08612
Maya Shpan Carmi <Maya.Shpan@bst.co.il>
2026-05-24 08:38:19 +0000

    add fields to tables

--- models/Budget_Control/silver/views/PROJ_BUD_EXP_FORECAST_V.sql
+++ models/Budget_Control/silver/views/PROJ_BUD_EXP_FORECAST_V.sql
@@ -2,7 +2,7 @@
 select 
     FORECAST_ID           as "צפי לגמר",
 	USER_ID               as "משתמש",
-	BUD_CONTROL_PERIOD_ID as "תקופה לבקרה",
+	BUD_CONTROL_PERIOD_ID as "בקרה תקציבית_ID",
 	CHAPTER_NAME          as "מספר פרק",
 	CHAPTER_DES           as "תיאור פרק",
 	SUB_CHAPTER_NAME      as "מספר תת פרק",
@@ -15,5 +15,6 @@ select
 	APPROVED_TO_PAY       as "מאושר לתשלום",
 	PREVIOUS_FORECAST     as "אומדן קודם (הוצאות)",
     ORIGINAL_BUDGET       as "תקציב הוצאות מקורי",
+    PROJECT_ID            as "מספר פרויקט",
 	SOURCE_DB             as "חברה"
 from {{ ref('PROJ_BUD_EXP_FORECAST') }}
