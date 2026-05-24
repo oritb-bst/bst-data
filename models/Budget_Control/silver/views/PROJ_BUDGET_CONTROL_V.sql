dafb9ef8e80d7cc98d4b8ccf229b7c24e52d9b9a
Maya Shpan Carmi <Maya.Shpan@bst.co.il>
2026-05-24 08:58:17 +0000

    change period name in proj_budget_control

--- models/Budget_Control/silver/views/PROJ_BUDGET_CONTROL_V.sql
+++ models/Budget_Control/silver/views/PROJ_BUDGET_CONTROL_V.sql
@@ -1,6 +1,6 @@
 --BUD_CONTROLPERIODS_Z
 select 
-    BUD_CONTROL_PERIOD_ID as "תקופה לבקרה",
+    BUD_CONTROL_PERIOD_ID as "בקרה תקציבית_ID",
     PROJECT_ID            as "פרויקט_ID",
     PROJECT_NAME          as "מספר פרויקט",
     BUD_CONTROL_MONTH     as "חודש בקרה",
