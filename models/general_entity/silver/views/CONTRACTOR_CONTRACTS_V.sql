41fa47f3f56c6d801a34c86d29925f8d3eb58610
Maya Shpan Carmi <Maya.Shpan@bst.co.il>
2026-05-25 09:41:56 +0000

    fix DATE

--- models/general_entity/silver/views/CONTRACTOR_CONTRACTS_V.sql
+++ models/general_entity/silver/views/CONTRACTOR_CONTRACTS_V.sql
@@ -6,7 +6,7 @@ select
 	PRICETOAPP      as "מצטבר חוזה",
 	MED_TYPE        as "סוג חוזה",
 	PRICE_AFTER_DIS as "מחיר אחרי הנחה",
-	Date ,
+	Date,
     ORDER_NAME      as "שם הזמנה",
     MED_TYPE_DESC   as "תיאור סוג חוזה",
 	SOURCE_DB       as "חברה"
