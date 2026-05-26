bbe0ee3936f12e3c7d2076e3f949cf1ea8ab1cca
Maya Shpan Carmi <Maya.Shpan@bst.co.il>
2026-05-26 12:41:19 +0000

    תיקון רווחים

--- models/general_entity/silver/views/PURCHASE_ORDERS_V.sql
+++ models/general_entity/silver/views/PURCHASE_ORDERS_V.sql
@@ -4,6 +4,6 @@ select
 	SUP_NAME     as "מספר ספק",
 	CURDATE      as "Date",
 	PRICE_AFTER_DIS_PORD as "מחיר הזמנת רכש אחרי הנחה",
-	STATUS_PORD          as "סטטוס הזמנת רכש" ,
+	STATUS_PORD          as "סטטוס הזמנת רכש",
 	SOURCE_DB            as "חברה"
 from {{ ref('PURCHASE_ORDERS') }}
