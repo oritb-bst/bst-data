22bcf0997c3910910153a4e753ed171039dcd365
Maya Shpan Carmi <Maya.Shpan@bst.co.il>
2026-05-26 07:41:22 +0000

    add table + change שם קבלן to מספר קבלן

--- models/general_entity/silver/views/CONTRACTOR_PARTIAL_INVOICES_V.sql
+++ models/general_entity/silver/views/CONTRACTOR_PARTIAL_INVOICES_V.sql
@@ -1,8 +1,9 @@
+--MED_DOCUMENTS_P
 select  
     PROJECT_ID          as "פרויקט_ID",
 	PROJECT_NAME        as "מספר פרויקט",
-	CONTRACTOR_NAME     as "שם קבלן",
-	CONTRACTOR_DES      as "תיאור קבלן",
+	SUP_NAME            as "מספר קבלן",
+	SUP_DES             as "תיאור קבלן",
 	PARTIAL_INVOICE_NUM as "מספר חשבון",
 	CURDATE             as "Date",
     PRICE_AFTER_DISCOUNT_INV as "מחיר חשבון אחרי הנחה",
