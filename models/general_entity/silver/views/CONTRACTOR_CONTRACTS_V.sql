ce2e2abd479ca230dc2c74cc2185666c59361a8f
Maya Shpan Carmi <Maya.Shpan@bst.co.il>
2026-05-27 06:55:15 +0000

    change מספר הזמנה in table CONTRACTOR_CONTRACTS

--- models/general_entity/silver/views/CONTRACTOR_CONTRACTS_V.sql
+++ models/general_entity/silver/views/CONTRACTOR_CONTRACTS_V.sql
@@ -7,7 +7,7 @@ select
 	MED_TYPE        as "סוג חוזה",
 	PRICE_AFTER_DIS_CON as "מחיר חוזה אחרי הנחה",
 	CURDATE         as "Date",
-    ORDER_NAME      as "שם הזמנה",
+    ORDER_NAME      as "מספר הזמנה",
     MED_TYPE_DESC   as "תיאור סוג חוזה",
     STATUS_CON      as "סטטוס חוזה",
 	SOURCE_DB       as "חברה"
