cd3b3df314211719467723037f87b3f684414e9d
Orit Barak <orit.b@bst.co.il>
2026-05-28 08:13:47 +0000

    ADDED LOAD TS TO FORECAST PROJECTS JASON

--- models/Income_Expenses/jsonextract/FORECAST_PROJECTS_J.sql
+++ models/Income_Expenses/jsonextract/FORECAST_PROJECTS_J.sql
@@ -7,7 +7,8 @@ SELECT
     item.value:EARNCAST::string      AS EARNCAST,
     item.value:TOTEXPENSE::string    AS TOTEXPENSE,
     item.value:FOREX::string         AS FOREX,
-    SOURCE_DB::string                AS SOURCE_DB
+    SOURCE_DB::string                AS SOURCE_DB,
+    CURRENT_TIMESTAMP()              AS LOAD_TS
 
 FROM {{ source('json', 'BST_FOREXSUB') }},
 LATERAL FLATTEN(input => DATA) item
