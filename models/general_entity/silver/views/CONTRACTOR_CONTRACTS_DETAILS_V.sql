b4506691b01648a25221af7fbf71a980c158699f
Maya Shpan Carmi <Maya.Shpan@bst.co.il>
2026-06-07 10:48:03 +0000

    הוספת מסך חדש MED_PORDERITEMS

---
+++ models/general_entity/silver/views/CONTRACTOR_CONTRACTS_DETAILS_V.sql
@@ -0,0 +1,9 @@
+--MED_PORDERITEMS_SUBFORM
+SELECT
+	CONTRACT_SECTION_NAME as "מספר סעיף חוזה",
+    CONTRACT_SECTION_DES  as "תיאור סעיף חוזה",
+    QUANTITY              as "כמות",
+    NET_UNIT_PRICE        as "מחיר ליח' נטו",
+    TOTAL_PRICE           as "סך הכל מחיר",
+	SOURCE_DB             as "חברה"
+FROM {{ ref('CONTRACTOR_CONTRACTS_DETAILS') }}
