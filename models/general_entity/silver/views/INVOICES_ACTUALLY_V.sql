42deb47096f1444a18025c9879c5bf1c68b79b6c
Orit Barak <orit.b@bst.co.il>
2026-06-18 11:53:48 +0000

    1

--- models/general_entity/silver/views/INVOICES_ACTUALLY_V.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    DOC_PROJECT as "מספר פרויקט",
-    INVOICE_DATE  as "Date",
-    INVOICES_ACTUALLY_NEW,
-    INVOICES_ACTUALLY_EXPENSE_NEW ,
-    SOURCE_DB  as "חברה"
-from {{ ref('INVOICES_ACTUALLY') }}
