43d80e0665d7e7e50d20eeee05ce2618483c6d85
Maya Shpan Carmi <Maya.Shpan@bst.co.il>
2026-06-18 12:44:26 +0000

    שינוי מבקר התקשרויות בילדאפ לבקר תקציב ישיר

--- models/Buildup/processing/DIM_PROJECT_ROLES_STG_Buildup.sql
+++ models/Buildup/processing/DIM_PROJECT_ROLES_STG_Buildup.sql
@@ -1,15 +1,15 @@
 --ZCBS_PROJPOSITIONS_SUBFORM
 --33 מנהל איזור
---42 בקר התקשרויות בילדאפ
+--55 בקר תקציב ישיר
 select
     DOCNO,
     DOC,
     max(case when POSITIONCODE = 33 then POSITIONCODE end) as AREA_MANAGER_POSITIONCODE,
     max(case when POSITIONCODE = 33 then USERNAME end)     as AREA_MANAGER_USERNAME,
-    max(case when POSITIONCODE = 42 then POSITIONCODE end) as BLDUP_CONTRACT_CONTROLLER_POSITIONCODE,
-    max(case when POSITIONCODE = 42 then USERNAME end)     as BLDUP_CONTRACT_CONTROLLER_USERNAME,
+    max(case when POSITIONCODE = 55 then POSITIONCODE end) as DIRECT_BUDGET_CONTROLLER_POSITIONCODE,
+    max(case when POSITIONCODE = 55 then USERNAME end)     as DIRECT_BUDGET_CONTROLLER_USERNAME,
     SOURCE_DB
 from {{ ref('ZCBS_PROJPOSITIONS_J') }}
-where POSITIONCODE in (33, 42)
+where POSITIONCODE in (33, 55)
   and SOURCE_DB = 'BLDUP'
 group by DOCNO, DOC, SOURCE_DB
