0443f860e31f583b958e32a1791806d5c6146f15
Maya Shpan Carmi <Maya.Shpan@bst.co.il>
2026-06-24 08:56:48 +0000

    שינוי מקרו שיהיה בלי גרשיים

--- macros/filter_last_n_years.sql
+++ macros/filter_last_n_years.sql
@@ -1,7 +1,7 @@
 -- סינון על 4 השנים האחרונות
 
 {% macro filter_last_n_years(date_field, years=4) %}
-    {{ adapter.quote(date_field) }} >= {{ dbt.dateadd(
+    {{ date_field }} >= {{ dbt.dateadd(
         datepart="year",
         interval=-years,
         from_date_or_timestamp=dbt.current_timestamp()
