2822c65d1f9e8fc27fec80c8e66cb01aaff88594
Maya Shpan Carmi <Maya.Shpan@bst.co.il>
2026-07-27 10:02:28 +0000

    הוספת ויו של הטבלת הוצאות בפועל (שעמית הכין מהפריוריטי) למודל בקרה תקציבית

---
+++ models/Budget_Control/silver/views/PROJ_ACTUAL_EXPENSES_V_Budget_Control.sql
@@ -0,0 +1,40 @@
+SELECT
+    a.source_db AS "חברה",
+    a.Control_Date AS "תאריך בקרה",
+    a.Project AS "מספר פרויקט",
+    a.Project_Desc AS "תאור פרויקט",
+    a.Sub_Chapter AS "תת פרק",
+    a.Sub_Chapter_Desc AS "תאור תת פרק",
+    a.Resource AS "משאב",
+    a.Resource_Desc AS "תאור משאב",
+    a.Doc_Type AS "סוג תעודה",
+    a.Doc_Desc AS "תאור תעודה",
+    a.Supplier_No AS "מס' ספק",
+    a.Supplier_Name AS "שם ספק",
+    a.Doc_No AS "מספר תעודה",
+    a.Exec_Month_Date AS "Date",
+    a.Part_No AS "מק'ט",
+    a.Part_Desc AS "תאור מוצר",
+    a.Doc_Quant AS "כמות בתעודה",
+    a.Factory_Unit AS "יח' מפעל",
+    a.Unit_Price AS "מחיר יח'",
+    a.Amount AS "סכום",
+    a."הוצאות בפועל בטון",
+    a."הוצאות בפועל ברזל",
+    a."הוצאות בפועל ניהול שונות",
+    a."הוצאות בפועל קבלנים",
+    a."הוצאות בפועל ניהול אתר",
+    a."הוצאות בפועל רכש",
+    a."הוצאות בפועל כמות בטון",
+    a."הוצאות בפועל כמות ברזל",
+    a."הוצאות בפועל כמות ניהול שונות",
+    a."הוצאות בפועל כמות קבלנים",
+    a."הוצאות בפועל כמות ניהול אתר",
+    a.Project_Expenses_Exclude_Flag,
+    a.Control_Month,
+    a.Entity_Name
+
+FROM {{ ref('PROJECT_ACTUAL_EXPENSES_STG') }} a
+
+
+{{ join_bst_projects_budget_control('a.Project', 'a.source_db') }}
