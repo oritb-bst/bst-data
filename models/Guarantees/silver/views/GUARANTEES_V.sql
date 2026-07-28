e3e11f6ed6d18d4fc3b8ec7225450cfd8e36707e
Maya Shpan Carmi <Maya.Shpan@bst.co.il>
2026-07-28 12:51:24 +0000

    הוספת טבלה חדשה MEDR_GUARANTEES

---
+++ models/Guarantees/silver/views/GUARANTEES_V.sql
@@ -0,0 +1,14 @@
+--MEDR_GUARANTEES
+select
+    PROJECT_NAME as "מספר פרויקט",
+    PROJECT_DES  as "שם פרויקט",
+    BANK_NAME    as "שם בנק",
+    GUARANTEE_TYPE_DES        as "תיאור סוג ערבות",
+    BANK_GUARANTEE_REFERENCE  as "מספר הערבות בבנק",
+    ACC_DES                   as "שם לקוח/ספק מקורי",
+    GUARANTEE_AMOUNT          as "סכום ערבות",
+    REVALUED_GUARANTEE_AMOUNT as "ערבות משוערכת",
+    GUARANTEE_START_DATE      as "תאריך תחילת ערבות",
+    GUARANTEE_END_DATE        as "תאריך תוקף מעודכן",
+    SOURCE_DB                 as "חברה"
+from {{ ref('MEDR_GUARANTEES') }}
