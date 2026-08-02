ff691d4f006f6cc2eb0fd799ade08a157fa92836
Maya Shpan Carmi <Maya.Shpan@bst.co.il>
2026-08-02 10:03:14 +0000

    שינוי שם שדה

--- models/general_entity/jsonextract/MEDR_GUARANTEES_J.sql
+++ models/general_entity/jsonextract/MEDR_GUARANTEES_J.sql
@@ -10,9 +10,9 @@ SELECT
     item.value:SDATE::date            AS SDATE,
     item.value:EXPDATE::date          AS EXPDATE,
     item.value:CDES::string           AS CDES,
-    item.value:GUARANTYPENAM::varchar AS GUARANTYPENAM,
-    item.value:STATDES::string        AS STATDES,
-    SOURCE_DB::string                 AS SOURCE_DB
+    item.value:GUARANTYPENAME::varchar AS GUARANTYPENAME,
+    item.value:STATDES::string         AS STATDES,
+    SOURCE_DB::string                  AS SOURCE_DB
 
 FROM {{ source('json', 'MEDR_GUARANTEES') }},
 LATERAL FLATTEN(input => DATA) item
