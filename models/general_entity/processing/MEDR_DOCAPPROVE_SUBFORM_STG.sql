51196b65301e2d87cf8f06a25e36947910f53e51
Ester Alishaeva <Esti.Alishaeva@bst.co.il>
2026-08-05 09:43:49 +0000

    added final income

--- models/general_entity/processing/MEDR_DOCAPPROVE_SUBFORM_STG.sql
+++ models/general_entity/processing/MEDR_DOCAPPROVE_SUBFORM_STG.sql
@@ -1,11 +1,7 @@
 --מסך בן MEDR_DOCAPPROVE - סכום מאושר לתקבול 
--- + מסך אב MED_DOCUMENTS_D
+
 
 SELECT
-    SUM(QPRICE) OVER (
-        PARTITION BY DOC
-    ) AS TOTAL_QPRICE,
-
     DOC,
     QPRICE,
     SOURCE_DB
