d5a9dc8f4fe279ef52ebb6b6407bf719ef4ac453
Maya Shpan Carmi <Maya.Shpan@bst.co.il>
2026-08-10 13:44:34 +0000

    הוספת סינון לSTG של הלוגים

--- models/general_entity/processing/GUARANTEES_CHANGE_LOG_STG.sql
+++ models/general_entity/processing/GUARANTEES_CHANGE_LOG_STG.sql
@@ -1,10 +1,13 @@
 --MEDR_GUARANTCHNGLOG_SUBFORM
 select
-    CHNGTYPE  as CHANGE_TYPE,
-    PREVVALUE as PREV_VALUE,
-    NEWVALUE  as NEW_VALUE,
-    STATDES   as STAT_IN_CHANGE,
-    UDATE     as CHANGE_DATE,
+    CHNGTYPE  as CHANGE_TYPE, --סוג שינוי
+    PREVVALUE as PREV_VALUE, --ערך ישן
+    NEWVALUE  as NEW_VALUE, --ערך חדש
+    STATDES   as STAT_IN_CHANGE, --סטטוס בזמן שינוי
+    UDATE     as CHANGE_DATE, --תאריך שינוי
     DOCNO     as GUARANTEE_NAME, --מספר ערבות
     SOURCE_DB
 from {{ ref('MEDR_GUARANTCHNGLOG_J') }}
+where CHNGTYPE = 'סכום ערבות'
+and PREVVALUE <> 0
+and STATDES = 'מאושרת'
