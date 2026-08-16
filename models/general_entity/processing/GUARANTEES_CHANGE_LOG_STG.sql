adee754a90584002c17ed49b4573d935877b813b
Maya Shpan Carmi <Maya.Shpan@bst.co.il>
2026-08-16 12:57:08 +0000

    שינוי בדגל ערבויות מסוג מקדמה שלא היו בהן הקטנה במשך 3 חודשים אחרונים

--- models/general_entity/processing/GUARANTEES_CHANGE_LOG_STG.sql
+++ models/general_entity/processing/GUARANTEES_CHANGE_LOG_STG.sql
@@ -1,15 +1,16 @@
---הטבלה הזאת לטובת בדיקה האם היתה הקטנה בסכום הערבות לערבויות מאושרות
+--הטבלה הזאת לטובת בדיקה האם היתה הקטנה בסכום הערבות לערבויות מאושרות ב3 חודשים האחרונים
 -- MEDR_GUARANTCHNGLOG_SUBFORM
 select
-    CHNGTYPE                 as CHANGE_TYPE, --סוג שינוי
-    TRY_TO_NUMBER(PREVVALUE) as PREV_VALUE, --ערך ישן
-    TRY_TO_NUMBER(NEWVALUE)  as NEW_VALUE, --ערך חדש
-    STATDES                  as STAT_IN_CHANGE, --סטטוס בזמן שינוי
-    TO_DATE(UDATE)           as CHANGE_DATE, --תאריך שינוי
-    DOCNO                    as GUARANTEE_NAME, --מספר ערבות
+    CHNGTYPE                  as CHANGE_TYPE, --סוג שינוי
+    TRY_TO_NUMBER(PREVVALUE ) as PREV_VALUE, --ערך ישן
+    TRY_TO_NUMBER(NEWVALUE)   as NEW_VALUE, --ערך חדש
+    STATDES                   as STAT_IN_CHANGE, --סטטוס בזמן שינוי
+    TO_DATE(UDATE)            as CHANGE_DATE, --תאריך שינוי
+    DOCNO                     as GUARANTEE_NAME, --מספר ערבות
     SOURCE_DB,
     case when TRY_TO_NUMBER(NEWVALUE) < TRY_TO_NUMBER(PREVVALUE) then 1 else 0 end as IS_DECREASE
 from {{ ref('MEDR_GUARANTCHNGLOG_J') }}
+
 where CHNGTYPE = 'סכום ערבות'
   and TRY_TO_NUMBER(PREVVALUE) <> 0.00
   and STATDES = 'מאושרת'
