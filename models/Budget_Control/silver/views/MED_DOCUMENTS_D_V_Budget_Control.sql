0d1c7eeabedfe29197e3e8a4963df2285bcc5a90
Maya Shpan Carmi <Maya.Shpan@bst.co.il>
2026-08-20 11:37:24 +0000

    שינוי במאקרו מפרויקט_ID למספר פרויקט

--- models/Budget_Control/silver/views/MED_DOCUMENTS_D_V_Budget_Control.sql
+++ models/Budget_Control/silver/views/MED_DOCUMENTS_D_V_Budget_Control.sql
@@ -1,21 +1,17 @@
 -- חשבון חלקי (מזמין)
 select
-    d.execution_date as "Date",
-    d.PROJECT_DOCNO  as "מספר פרויקט",
-    d.DOC,
-    d.ORDNAME        as "מספר חוזה מזמין",
-    d.CUSTNAME       as "מס. לקוח",
-    d.DISPRICE       as "סכום מוגש - מחיר אחרי הנחה",
-    d.EXPECTPAY      as "סכום תשלום צפוי",
-    d.STATDES        as "סטטוס חשבון",
-    d.BOOKNUM        as "מספר חשבון חלקי",
-    d.SOURCE_DB      as "חברה",
-    coalesce(nullif(d.EXPECTPAY/1000, 0), d.DISPRICE/1000) as "סכום תשלום",
+    execution_date as "Date",
+    PROJECT_DOCNO  as "מספר פרויקט",
+    DOC,
+    ORDNAME        as "מספר חוזה מזמין",
+    CUSTNAME       as "מס. לקוח",
+    DISPRICE       as "סכום מוגש - מחיר אחרי הנחה",
+    EXPECTPAY      as "סכום תשלום צפוי",
+    STATDES        as "סטטוס חשבון",
+    BOOKNUM        as "מספר חשבון חלקי",
+    t.SOURCE_DB      as "חברה",
+    coalesce(nullif(EXPECTPAY/1000, 0), DISPRICE/1000) as "סכום תשלום",
 
-    p."פרויקט_ID"
+from {{ ref('MED_DOCUMENTS_D_STG') }} t
 
-from {{ ref('MED_DOCUMENTS_D_STG') }} d
-
-inner join {{ ref('DIM_PROJECTS_V_Budget_Control') }} p
-    on d.PROJECT_DOCNO = p."מספר פרויקט"
-   and d.SOURCE_DB = p."חברה"
+{{ join_bst_projects_budget_control('PROJECT_DOCNO', 't.SOURCE_DB') }}
