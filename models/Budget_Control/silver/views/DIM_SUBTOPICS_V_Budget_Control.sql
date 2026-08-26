46f64e9c8000224caad6ee42238df12887d2bcbb
Maya Shpan Carmi <Maya.Shpan@bst.co.il>
2026-08-26 07:18:43 +0000

    תיקונים

--- models/Budget_Control/silver/views/DIM_SUBTOPICS_V_Budget_Control.sql
+++ models/Budget_Control/silver/views/DIM_SUBTOPICS_V_Budget_Control.sql
@@ -1,6 +1,6 @@
 -- COSTCENTERS4Q משאבים
 select
-    SUB_CHAPTER_NAME as "מספר משאב",
-    SUB_CHAPTER_NAME as "תיאור משאב",
-    SOURCE_DB        as "חברה"
+    SUB_TOPIC_NAME as "מספר משאב",
+    SUB_TOPIC_DES  as "תיאור משאב",
+    SOURCE_DB      as "חברה"
 from {{ ref('DIM_SUBTOPICS_STG') }} 
