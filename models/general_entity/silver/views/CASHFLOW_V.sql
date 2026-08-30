720f2a974e9867f67be8256ad9b77c6bad4f9976
Orit Barak <orit.b@bst.co.il>
2026-08-30 12:08:45 +0000

    באלפי שח

--- models/general_entity/silver/views/CASHFLOW_V.sql
+++ models/general_entity/silver/views/CASHFLOW_V.sql
@@ -6,9 +6,13 @@ SELECT
     b.INVOICE_DATE                           AS "Date", --תאריך התקבול
     b.BALDATE                                AS "ת. למאזן",
     b.TOTPRICE                               AS "סה""כ קבלה",
+    b.TOTPRICE  /1000                        AS "סה""כ קבלה באלפי שח",
     b.TOTPRICEIV                             AS "סכום החשבונית",
+    b.TOTPRICEIV /1000                       AS "סכום החשבונית באלפי שח",
     b.TOTKEREN                               AS "סכום הקרן",
+    b.TOTKEREN /1000                         AS "סכום הקרן באלפי שח",
     b.PEREXTRACTION                          AS "מע""מ",
+    b.PEREXTRACTION /1000                    AS "מע""מ באלפי שח",
     b.RINCIPALVATCODE                        AS "קוד אופן תשלום",
     b.RINCIPALVATDES                         AS "תיאור אופן תשלום",
     b.SOURCE_DB                              AS "חברה"
