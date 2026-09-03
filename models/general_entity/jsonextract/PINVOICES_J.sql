c2897f037f3aac9093325958f4fa38646a2ca836
Orit Barak <orit.b@bst.co.il>
2026-09-03 08:02:40 +0000

    new pinvoices

--- models/general_entity/jsonextract/PINVOICES_J.sql
+++ models/general_entity/jsonextract/PINVOICES_J.sql
@@ -6,11 +6,14 @@ SELECT
     item.value:QPRICE::float         AS QPRICE,
     item.value:DISPRICE::float       AS DISPRICE,
     item.value:TOTPRICE::float       AS TOTPRICE,
+    item.value:CALPRICE::float       AS CALPRICE,
     item.value:STORNOFLAG::string    AS STORNOFLAG,
     item.value:SUPNAME::string       AS SUPNAME,
     item.value:FINAL::string         AS FINAL,
     item.value:STATDES::string       AS STATDES,
     item.value:ORDNAME::string       AS ORDNAME,
+    item.value:DEBIT::string        AS DEBIT,
+    item.value:DOCNO::string        AS DOCNO,    
     SOURCE_DB::string                AS SOURCE_DB
 
 FROM {{ source('json', 'PINVOICES') }},
