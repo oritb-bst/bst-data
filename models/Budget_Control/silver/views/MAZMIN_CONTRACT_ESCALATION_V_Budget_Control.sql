4b280846a5a7fcb7f26aea304fce344fab632932
Maya Shpan Carmi <Maya.Shpan@bst.co.il>
2026-09-15 10:29:59 +0000

    שינוי שדות הצמדה

--- models/Budget_Control/silver/views/MAZMIN_CONTRACT_ESCALATION_V_Budget_Control.sql
+++ models/Budget_Control/silver/views/MAZMIN_CONTRACT_ESCALATION_V_Budget_Control.sql
@@ -2,9 +2,9 @@
 select
     PROJECT_NAME          as "מספר פרויקט",
     ORD_NAME              as "מספר חוזה מזמין",
-    CURRENCY              as "מטבע",
+    CURRENCY_LINKAGE      as "מטבע הצמדה",
     ESCALATION_START_DATE as "תאריך תחילת הצמדה",
     BASE_RATE             as "שער בסיס",
-    CURRENCY_DES          as "סוג הצמדה",
+    CURRENCY_LINKAGE_DES  as "תיאור מטבע הצמדה",
     SOURCE_DB             as "חברה"
 FROM {{ ref('MAZMIN_CONTRACT_ESCALATION_STG') }}
